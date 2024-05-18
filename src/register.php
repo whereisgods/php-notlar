4bdbcfad2e8bfd7175791157691af3a7e96d28df
allahin <166995789+allahin@users.noreply.github.com>
2024-05-18 07:46:52 +0300

    fix

--- src/register.php
+++ src/register.php
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $_SESSION['user_id'] = mysqli_insert_id($conn);
 
                 // Kullanıcıyı hoş geldiniz sayfasına yönlendirin
-                header("Location: ./login");
+                header("Location: ./");
                 exit();
             } else {
                 $errors[] = "Kayıt sırasında bir hata oluştu: " . mysqli_error($conn);
